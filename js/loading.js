645fe14ec1089e7498a02ecce41a6db4c5cc16a3
leanhtuandev <leanhtuan.yb29@gmail.com>
2021-01-14 17:03:47 +0700

    update

--- js/loading.js
+++ js/loading.js
@@ -1,30 +1,32 @@
 
-function onReady(callback) {
-    var intervalId = window.setInterval(function() {
-      if (document.getElementsByTagName('body')[0] !== undefined) {
-        window.clearInterval(intervalId);
-        callback.call(this);
-      }
-    }, 1000);
-  }
+// function onReady(callback) {
+//     var intervalId = window.setInterval(function() {
+//       if (document.getElementsByTagName('body')[0] !== undefined) {
+//         window.clearInterval(intervalId);
+//         callback.call(this);
+//       }
+//     }, 1000);
+//   }
   
-  function setVisible(selector, visible) {
-    document.querySelector(selector).style.display = visible ? 'block' : 'none';
-  }
-  onReady(function() {
-    setVisible('#navbar', true);
-    setVisible('#banner', true);
-    setVisible('#mockup', true);
-    setVisible('#feature', true);
-    setVisible('#translation-assistance', true);
-    setVisible('#feed-back', true);
-    setVisible('#buy-now', true);
-    setVisible('#contact', true);
-    setVisible('#map', true);
-    setVisible('#footer', true);
-    setVisible('#loading', false);
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0; 
-  });
+//   function setVisible(selector, visible) {
+//     document.querySelector(selector).style.display = visible ? 'inline-block' : 'none';
+//   }
+//   onReady(function() {
+//     setVisible('#navbar', true);
+//     setVisible('#banner', true);
+//     setVisible('#mockup', true);
+//     setVisible('#feature', true);
+//     setVisible('#translation-assistance', true);
+//     setVisible('#feed-back', true);
+//     setVisible('#buy-now', true);
+//     setVisible('#content', true);
+//     setVisible('#develope', true);
+//     setVisible('#map', true);
+//     setVisible('#footer', true);
+//     setVisible('#loading', false);
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+    
+//   });
 
   
